6600a00e104aadb6b7000b1d149ae48b5778149f
leee1 <ErikSLee@gmail.com>
2017-02-17 07:39:46 -0600

    test1

--- Mecanum Drive/src/org/usfirst/frc/team2830/robot/Robot.java
+++ Mecanum Drive/src/org/usfirst/frc/team2830/robot/Robot.java
@@ -14,7 +14,7 @@ public class Robot extends SampleRobot {
 	
     RobotDrive robotDrive;
     Joystick stick;
-
+//change 1 thing
     // Channels for the wheels
     final int frontLeftChannel	= 2;
     final int rearLeftChannel	= 3;
